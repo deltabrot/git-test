0664b094e8e1d33c94af5cfca091acd69ba3d7b9
Jamie Scollay <jamie@scollay.uk>
2022-09-20 17:14:59 +0100

    Added a farewell message

--- hello.py
+++ hello.py
@@ -8,3 +8,5 @@ print("You are " + age + " years old.")
 # Calculate how long until the user is 50 years old
 years = 50 - int(age)
 print("You will be 50 in " + str(years) + " years.")
+
+print("Farewell")
