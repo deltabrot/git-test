444d2416d8d7c91233206f37a70187982df458c2
Jamie Scollay <jamie@scollay.uk>
2022-09-20 17:02:54 +0100

    Ask users age and determine when they will turn 100

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 name = input("What is your name? ")
 print("Hello, " + name + "!")
+
+age = input("How old are you? ")
+print("You are " + age + " years old.")
+
+# Calculate how long until the user is 100 years old
+years = 100 - int(age)
+print("You will be 100 in " + str(years) + " years.")
