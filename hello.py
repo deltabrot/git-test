7e61f013af9e087ee0582160e1c9f0b19cd98b9b
Jamie Scollay <jamie@scollay.uk>
2022-09-20 17:05:29 +0100

    Changed age to 50 years old

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ print("Hello, " + name + "!")
 age = input("How old are you? ")
 print("You are " + age + " years old.")
 
-# Calculate how long until the user is 100 years old
-years = 100 - int(age)
-print("You will be 100 in " + str(years) + " years.")
+# Calculate how long until the user is 50 years old
+years = 50 - int(age)
+print("You will be 50 in " + str(years) + " years.")
